5923e17bc7bb15684b8bcf6b840bb7c40e489d1b
mitch0 <mitch0@localhost>
2010-09-13 23:45:53 +0200

    fix drycode errors

--- Broker_WhoLib.lua
+++ Broker_WhoLib.lua
@@ -8,6 +8,7 @@ local AppName = "Broker_WhoLib"
 local Icon = [[Interface\AddOns\]] .. AppName .. [[\icon.tga]]
 
 local tmp = {}
+local ttFrame, ttOwner
 local QueueNames = {
         ' USER',
         ' QUIET',
@@ -36,15 +37,15 @@ local function getTimeInMinutes(since)
 	return tostring(math.floor(minutes))
 end
 
-local tooltip = CreateFrame("GameTooltip", AppName .. "_Tooltip", UIParent)
-function tooltip:OnShow()
+local function updateTooltip(tooltip)
+    ttFrame, ttOwner = tooltip, tooltip:GetOwner()
     WhoLib:UpdateWeights()
-    self:ClearLines()
-    self:AddLine('|cff00ff00WhoLib Queues:|r')
+    tooltip:ClearLines()
+    tooltip:AddLine('|cff00ff00WhoLib Queues:|r')
     for num, name in ipairs(QueueNames) do
-        self:AddLine('|cffffff00' .. name .. '|r (' .. (math.floor(WhoLib.queue_bounds[num]*100)) .. '% chance)')
+        tooltip:AddLine('|cffffff00' .. name .. '|r (' .. (math.floor(WhoLib.queue_bounds[num]*100)) .. '% chance)')
         if(#WhoLib.Queue[num] == 0) then
-            self:AddLine('|cff888888empty|r')
+            tooltip:AddLine('|cff888888empty|r')
         else
             for _, v in ipairs(WhoLib.Queue[num]) do
                 local text
@@ -57,7 +58,7 @@ function tooltip:OnShow()
                 else
                     text = 'Who: ' .. v.query
                 end
-                self:AddLine(text)
+                tooltip:AddLine(text)
             end
         end
     end
@@ -68,28 +69,28 @@ function tooltip:OnShow()
     end
 
     if #tmp == 0 then
-        self:AddLine('|cff00ff00WhoLib Cache:|r')
-        self:AddLine('|cff888888empty|r')
+        tooltip:AddLine('|cff00ff00WhoLib Cache:|r')
+        tooltip:AddLine('|cff888888empty|r')
     else
-        self:AddLine(('|cff00ff00WhoLib Cache: %s entries|r'):format(tostring(#tmp)))
+        tooltip:AddLine(('|cff00ff00WhoLib Cache: %s entries|r'):format(tostring(#tmp)))
         table.sort(tmp, sortCache)
         local data
         for i, name in ipairs(tmp) do
             data = WhoLib.Cache[name]
             tmp[i] = nil
             if data.inqueue then
-                self:AddLine(('%s : |cff8888FFquerying... (%s min)|r'):format(name, getTimeInMinutes(data.last)))
+                tooltip:AddLine(('%s : |cff8888FFquerying... (%s min)|r'):format(name, getTimeInMinutes(data.last)))
             else
                 if data.valid then
                     if data.data.Online then
                         local sex = data.data.Sex and (data.data.Sex==2 and MALE or FEMALE) or ""
-                        self:AddLine(('%s : |cff88FF88%s - %s %s  (%s min)|r'):format(name, tostring(data.data.Level), sex, tostring(data.data.Class), getTimeInMinutes(data.last)))
+                        tooltip:AddLine(('%s : |cff88FF88%s - %s %s  (%s min)|r'):format(name, tostring(data.data.Level), sex, tostring(data.data.Class), getTimeInMinutes(data.last)))
                     else
                         local status = data.Online == nil and "not found" or "offline"
-                        self:AddLine(('%s : |cff88FF88%s  (%s min)|r'):format(name, status, getTimeInMinutes(data.last)))
+                        tooltip:AddLine(('%s : |cff88FF88%s  (%s min)|r'):format(name, status, getTimeInMinutes(data.last)))
                     end
                 else
-                    self:AddLine(('%s : |cff888888no info|r'):format(name))
+                    tooltip:AddLine(('%s : |cff888888no info|r'):format(name))
                 end
             end
         end
@@ -101,7 +102,7 @@ local BWL = {
     label = AppName,
     text = "",
     icon = Icon,
-    tooltip = tooltip,
+    OnTooltipShow = updateTooltip,
 }
 
 local function update()
@@ -111,8 +112,9 @@ local function update()
     end
 
     BWL.text = #WhoLib.Queue[1] .. '/' .. #WhoLib.Queue[2] .. '/' .. #WhoLib.Queue[3] .. ":" .. queries .. (WhoLib.WhoInProgress and " ? " or " - ") .. WhoLib:GetQueryInterval() .. "s"
-    if tooltip:IsShown() then
-        tooltip:OnShow()
+    if ttFrame and ttFrame:IsShown() and ttFrame:GetOwner() == ttOwner then
+       updateTooltip(ttFrame)
+       ttFrame:Show()
     end
 end
 
@@ -127,4 +129,5 @@ end
 
 WhoLib.RegisterCallback(BWL, 'WHOLIB_QUERY_ADDED', update)
 WhoLib.RegisterCallback(BWL, 'WHOLIB_QUERY_RESULT', update)
+update()
 LDB:NewDataObject(AppName, BWL)
